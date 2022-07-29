3144cbc254f72fe618446cf575e63e6a160ce8d6
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-07-29 08:52:24 -0300

    Código do segundo triângulo incompleto

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
   controlRight -= 1;
   controlLeft += 1
 };
+
+
+let num = 20;
+let middle = (num + 1) / 2;
+let controlLeft2 = middle;
+let controlRight2 = middle;
+let symbol2 = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft2 || col == controlRight2 || line == middle) {
+      outputLine += symbol2;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft2 -= 1;
+  controlRight2 += 1;
+  console.log(outputLine);
+}
