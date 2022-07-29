58fff8c1d84f558fb1b31bb1196cadf2069e50ba
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-07-29 18:20:11 -0300

    Finalizada criação da ampulheta e editado arquivo readme.

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Não deve fazer uso de NENHUMA biblioteca auxiliar de desenhos.
 
 ### Experiência 🧑‍🔧 
 - Achei bastante desafiador o desafio, não consegui fazer as laterais da ampulheta e nem fazer dois triângulos em um único código. Eu consegui fazer em códigos diferentes. O código comentado é o código do triângulo vazio por dentro e de cabeça pra cima.
+- Entender que para fazer os 2 "triângulos" da ampulheta seria necessário criar variáveis de controle correndo a matriz de dentro para fora e de fora para dentro.
 
 ### Explicação do código 💻
--
+- Nos comentários do código.
--- index.js
+++ index.js
@@ -1,42 +1,65 @@
-// Código completo do triângulo preenchido e de cabeça para baixo
+// Tamanho da ampulheta
 let n = 20;
+// Símbolo do desenho
 let symbol = '#';
+// Varíavel a ser imprimida no console
 let inputLine = '';
 
-let midOfMatrix = (n + 1) / 2; 
+// Índice central
+let midOfMatrix = ((n + 1) / 2)-1; 
+
+// Topo da ampulheta (triângulo invertido preenchido superior)
+
+// Variáveis de controle esquerda/direita para correr matriz de fora para dentro
 let controlLeft = midOfMatrix-(n/2);
 let controlRight = midOfMatrix+(n/2);
 
-for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex > controlLeft && columnIndex < controlRight) {
+// Loop das primeiras 10 linhas da matriz
+for (let lineIndex = 0; lineIndex < midOfMatrix; lineIndex += 1) {
+  // Loop das colunas em cada linha  
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex === 0 || columnIndex === (n-1)) { // Código para plotar o símbolo na primeira e última coluna de cada linha
       inputLine = inputLine + symbol;
-    } else {
-      inputLine = inputLine + ' ' ;
-    }
+    } else { // Código para desenhar o miolo do topo da ampulheta
+      if (columnIndex > controlLeft && columnIndex < controlRight) {
+        inputLine = inputLine + symbol;
+      } else {
+        inputLine = inputLine + ' ' ;
+    }}
   }
-  console.log(inputLine);
-  inputLine = '';
-  controlRight -= 1;
-  controlLeft += 1
+  console.log(inputLine); // Código para plotar linha
+  inputLine = ''; // Resetando inputline
+  controlRight -= 1; // Decrementando variável de controle da direita para o centro
+  controlLeft += 1 // Incrementando variável de controle da esquerda para o centro
 };
 
-// Código incompleto do triângulo vazio e de cabeça pra cima
-let num = 20;
-let middle = (num + 1) / 2;
-let controlLeft2 = middle;
-let controlRight2 = middle;
-let symbol2 = '*';
-for (let line = 1; line <= middle; line += 1) {
-  let outputLine = '';
-  for (let col = 1; col <= n; col += 1) {
-    if (col == controlLeft2 || col == controlRight2 || line == middle) {
-      outputLine += symbol2;
-    } else {
-      outputLine += ' ';
-    }
+// Base da ampulheta (triângulo vazado inferior)
+
+// Variáveis de controle esquerda/direita para correr matriz de dentro para fora
+controlLeft = midOfMatrix-1;
+controlRight = midOfMatrix+1;
+
+// Loop das linhas 11 a 19 da matriz
+for (let lineIndex = (n/2); lineIndex < n-1; lineIndex += 1) {
+  // Loop das colunas em cada linha  
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex === 0 || columnIndex === (n-1)) { // Código para plotar o símbolo na primeira e última coluna de cada linha
+      inputLine = inputLine + symbol;
+    } else { // Código para desenhar o miolo da base da ampulheta
+      if ((columnIndex === (controlLeft+((n/2)-midOfMatrix))) || (columnIndex === (controlRight-((n/2)-midOfMatrix)))) {
+        inputLine = inputLine + symbol;
+      } else {
+        inputLine = inputLine + ' ' ;
+    }}
   }
-  controlLeft2 -= 1;
-  controlRight2 += 1;
-  console.log(outputLine);
+  console.log(inputLine); // Código para plotar linha
+  inputLine = ''; // Resetando inputline
+  controlRight += 1; // Incrementando variável de controle do centro para direita
+  controlLeft -= 1; // Decrementando variável de controle do centro para esquerda
+};
+
+// Código para plotar última linha
+for(let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+  inputLine += symbol;
 }
+console.log(inputLine);
